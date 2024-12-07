d5c3c714ee5406de75805c8a0bebe05893e758bd
Petr Tripolsky <tripolskypetr@gmail.com>
2024-12-07 19:56:54 +0500

    sse-wip

--- README.md
+++ README.md
@@ -4,11 +4,38 @@
 
 ![screencast](./screencast.gif)
 
-## Usage
+## Test case 1: console-grpc-streaming
+
+> Console apps which stream events to each other through gRPC
+
+**Setup**
+
+No setup required
+
+**Running**
 
 ```bash
 npm install
 npm run build
-node .\services\msg-client-service\build\index.mjs
-node .\services\msg-server-service\build\index.mjs
+node ./services/msg-client-service/build/index.mjs
+node ./services/msg-server-service/build/index.mjs
+```
+
+## Test case 2: sse-grpc-streaming
+
+> HTTP/2 Server-Sent Events frontend app which recieves messages from gRPC service
+
+**Setup**
+```bash
+npm install
+npm run build
+cd ./apps/host-sse/ssl
+mkcert localhost # choco install mkcert
+```
+
+**Running**
+
+```bash
+node ./apps/host-sse/build/index.mjs
+npx-y open-cli https://localhost
 ```
--- apps/host-sse/src/index.ts
+++ apps/host-sse/src/index.ts
@@ -1,7 +1,27 @@
-import { grpc } from '@modules/remote-grpc';
+import { Hono } from 'hono'
 
-import "./__test__/foo-service.test";
-import "./__test__/bar-service.test";
-import "./__test__/baz-service.test";
+import { serve } from '@hono/node-server'
+import { serveStatic } from '@hono/node-server/serve-static'
 
-grpc.loggerService.setPrefix("host-sse");
+import http2 from 'http2'
+import fs from "fs";
+
+import realtime from './routes/realtime';
+
+const app = new Hono();
+
+app.route('/api/v1/realtime', realtime);
+
+app.use('/*', serveStatic({ root: './public' }))
+
+serve({
+  fetch: app.fetch,
+  createServer: http2.createSecureServer,
+  serverOptions: {
+    key: fs.readFileSync("./ssl/localhost-key.pem"),
+    cert: fs.readFileSync("./ssl/localhost.pem"),
+  },
+  port: 443,
+});
+
+console.log("Server listening on https://localhost:443");
